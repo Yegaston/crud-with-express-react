eeee28f3fd96040a0b061ce151dcf3172fd1e536
Gaston Graciani <gastongraciani@gmail.com>
2019-01-04 15:53:04 -0300

    Crud finish

--- src/app/App.js
+++ src/app/App.js
@@ -2,10 +2,108 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
-    addTask(e) {
+    constructor(){
+        super();
+        this.state = {
+            title: '',
+            description: '',
+            tasks: [],
+            id: '',
+        };
+        this.addTask = this.addTask.bind(this);
+        this.handleChange = this.handleChange.bind(this)
+        this.fetchTask = this.fetchTask.bind(this)
+    }
 
-        console.log('Adding task');
+    addTask(e) {
+        if(this.state.id){
+            fetch(`/api/tasks/${this.state.id}`, {
+                method: 'PUT',
+                body: JSON.stringify(this.state),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(res => res.json)
+                .then(data => {
+                    console.log(data)
+                    M.toast({html: 'Updated'});
+                    this.setState({title: '', description: '', id: ''})
+                    this.fetchTask();
+                })
+
+        } else {
+            fetch('/api/tasks',
+        {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                M.toast({html: "task saved"})
+                this.setState({title: '', description: ''});
+                this.fetchTask();
+
+            })
+            .catch(err => console.log(err));
+
+        }
+
         e.preventDefault();
+    }
+
+    handleChange(e){
+        const {name, value} = e.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    async fetchTask(){
+        const res = await fetch('/api/tasks');
+        const data = await res.json();
+
+        this.setState({ tasks: data});
+        console.log(this.state.tasks);
+    }
+
+    deleteTask(id){
+      if(confirm('Are you sure you want deleted that task??')){
+        fetch(`/api/tasks/${id}`,{
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+
+        })
+            .then(res => res.json)
+            .then(data => {
+                console.log(data);
+                M.toast({html: 'task deleted'})
+                this.fetchTask();
+            })
+      }   
+    }
+
+    async editTask(id){
+        const res = await fetch(`/api/tasks/${id}`);
+        const data = await res.json();
+        this.setState({
+            title: data.title,
+            description: data.description,
+            id: data._id
+        })
+    }
+
+    componentDidMount(){
+        this.fetchTask();
     }
     
     render() {
@@ -26,12 +124,12 @@ class App extends Component {
                                     <form onSubmit={this.addTask}>
                                         <div className="row">
                                             <div className="input-field col s12">
-                                                <input type="text" placeholder="Task title" />
+                                                <input name="title" onChange={this.handleChange} type="text" placeholder="Task title" value={this.state.title} />
                                             </div>
                                         </div>
                                         <div className="row">
                                             <div className="input-field col s12">
-                                              <textarea placeholder="Task description" className="materialize-textarea"></textarea>
+                                              <textarea onChange={this.handleChange} name="description" placeholder="Task description" className="materialize-textarea" value={this.state.description}></textarea>
                                             </div>
                                         </div>
                                         <button type="submit" className="btn light-blue darken-4">
@@ -42,7 +140,37 @@ class App extends Component {
                             </div>
                         </div>
                         <div className="col s7">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Title</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.tasks.map(task => {
+            return (
+                <tr key={task._id}>
+                    <td>{task.title}</td>
+                    <td>{task.description}</td>
+                    <td>
+                        <button className='btn ligh-blue darken-4'
+                            onClick={() => this.deleteTask(task._id)}>
+                            <i className='material-icons'>delete</i>
+                        </button>
+                        <button className='btn ligh-blue darken-4' style={{margin: '4px'}}
+                            onClick={() => this.editTask(task._id)}>
+                            <i className="material-icons">edit</i>
+                        </button>
+                    </td>
+                </tr>
+            )
+                                        })
+                                    }
 
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -4,20 +4,27 @@ const Task = require('../models/task')
 
 router.get('/', async (req, res, next) => {
     try {
-        tasks = await Task.find()
+        const tasks = await Task.find()
         res.json(tasks)
     }
-    catch(err){
+    catch (err) {
         console.log(err)
     }
-    
+
 });
+
+router.get('/:id', async (req, res, next) => {
+
+    const task = await Task.findById(req.params.id);
+    res.json(task);
+
+})
 
 router.post('/', async (req, res, next) => {
     console.log(req.body);
-    
-    const {title, description } = req.body
-    const task = new Task({title, description });
+
+    const { title, description } = req.body
+    const task = new Task({ title, description });
     console.log(task)
     try {
         await task.save()
@@ -25,20 +32,20 @@ router.post('/', async (req, res, next) => {
             status: "Received"
         })
     }
-    catch(err) {
+    catch (err) {
         res.json(err)
     }
 })
 
 router.put('/:id', async (req, res, next) => {
-    const {title, description } = req.body;
+    const { title, description } = req.body;
     const task = ({ title, description });
     console.log(task)
     try {
         await Task.findByIdAndUpdate(req.params.id, task);
-        res.json({status: "task updated"});
+        res.json({ status: "task updated" });
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 })
@@ -46,9 +53,9 @@ router.put('/:id', async (req, res, next) => {
 router.delete('/:id', async (req, res, next) => {
     try {
         await Task.findByIdAndDelete(req.params.id);
-        res.json({status: "Task deleted."});
+        res.json({ status: "Task deleted." });
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 })
